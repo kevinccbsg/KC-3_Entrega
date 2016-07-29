73a441995f0c3a592fed92e49ccb3d7d5f7e182b
kevinccbsg <kevin2906@hotmail.com>
2016-07-29 20:31:10 +0200

    Testing pagination next and previous

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -4,21 +4,49 @@ const utilitiesPagination = require('./utilitiesPagination');
 let numberItems = $('.story-section').length;
 let contentItems = $('.story-section');
 let linkPagination = $('.pagination > li a');
+let nextPage = $('#next a');
+let previousPage = $('#previous a');
 let pages = 4;
 let step = numberItems / pages;
 let itemsPerpage = Math.round(step);
 let targetToGo = $('#promoHeader');
-
+let numberPage = 1;
 let arrayItemsPerPage = utilitiesPagination.createArrayToPaginate(pages, itemsPerpage, contentItems);
 let copyContentItems = $('.story-section');
+
+utilitiesPagination.setPaginate(numberPage, step, numberItems, copyContentItems, arrayItemsPerPage);
 
 linkPagination.on('click', function (ev) {
 	ev.preventDefault();
 	utilitiesPagination.removeActive($('.pagination > li'));
 	$(this).parent().addClass('active');
-	let numberPage = parseInt($(this).text());
+	numberPage = parseInt($(this).text());
+	if (!isNaN(numberPage)) {
+		utilitiesPagination.setPaginate(numberPage, step, numberItems, copyContentItems, arrayItemsPerPage);
+		$('html, body').animate({
+			scrollTop: targetToGo[0].offsetHeight - 60
+		});
+	}
+});
+
+nextPage.on('click', function (ev) {
+	ev.preventDefault();
+	console.log("entro");
+	$(this).attr('class','');
+	numberPage++;
 	utilitiesPagination.setPaginate(numberPage, step, numberItems, copyContentItems, arrayItemsPerPage);
 	$('html, body').animate({
 		scrollTop: targetToGo[0].offsetHeight - 60
 	});
+});
+
+previousPage.on('click', function (ev) {
+	ev.preventDefault();
+	$(this).attr('class','');
+	numberPage--;
+	utilitiesPagination.setPaginate(numberPage, step, numberItems, copyContentItems, arrayItemsPerPage);
+	$('html, body').animate({
+		scrollTop: targetToGo[0].offsetHeight - 60
+	});
+	$(this).attr('class','disabled');
 });
