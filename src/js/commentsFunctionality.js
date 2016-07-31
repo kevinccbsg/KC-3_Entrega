bfda290205935d9971f5bec2c40329ec84520103
kevinccbsg <kevin2906@hotmail.com>
2016-07-31 13:54:09 +0200

    Improve UX comment form

--- src/js/commentsFunctionality.js
+++ src/js/commentsFunctionality.js
@@ -6,6 +6,7 @@ let textareaInput = document.getElementById('commentMessage');
 let nameInput = document.getElementById('inputName');
 let lasNameInput = document.getElementById('inputLastName');
 let emailInput = document.getElementById('inputEmail');
+let inputs = $('.form-group input, .form-group textarea');
 let errorBlock = document.querySelector('#error-message.block-content');
 let errorText = document.getElementById('error-text');
 
@@ -13,7 +14,6 @@ formComments.on('submit', function (ev) {
 	ev.preventDefault();
 	let textAreaText = textareaInput.value;
 	let numberCaracters = textAreaText.split(" ");
-	console.log(numberCaracters.length);
 	if (numberCaracters.length > 120) {
 		validationUtilities.errorSubmit('Max. 120 Characters', errorBlock, errorText);
 		validationUtilities.helperValidity(textareaInput, ev);
@@ -46,4 +46,8 @@ formComments.on('submit', function (ev) {
 			return false;
 		}
 	}
+});
+
+inputs.on('keydown', function (ev) {
+	$(this)[0].style.borderColor = '#f5f5f5';
 });
